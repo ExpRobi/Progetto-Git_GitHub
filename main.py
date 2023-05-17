69ff70dd34c674bff073c781d86b090748c73118
ExpRobi <romano.roberto.06@itisdalmine.edu.it>
2023-05-17 08:42:25 +0200

    aggiunti file main

---
+++ main.py
@@ -0,0 +1,15 @@
+import os
+
+""" fare un programma in python che smista i rifiuti è i rifiuti smaltiti vengono salvati in un file in modalità (append) con timestamp della data corrente alla richiesta
+
+    R1-Grande bianco freddo -grandi elettrodomestici per la refrigerazione: frigoriferi, congelatori, condizionatori
+    R2-Grande bianco non freddo -grandi elettrodomestici come lavatrici, lavastoviglie.
+    R3-TV Monitor a tubo catodico
+    R4-Elettronica di consumo, Telecomunicazioni, Informatica, piccoli elettrodomestici, elettroutensili, giocattoli, apparecchi di illuminazione, dispositivi medici.
+    R5-Sorgenti luminose a scarica: lampade fluorescenti e sorgenti luminose compatte.
+
+
+    il programma deve avere il commit automatico sulla reposotory di github chiamata "Progetto-Git_GitHub"
+
+
+"""
