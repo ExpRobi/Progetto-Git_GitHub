8efd6452b29181f766b2f01c323ee31cb29f6ffe
YoussefElmarzouki05 <133850307+YoussefElmarzouki05@users.noreply.github.com>
2023-05-30 11:53:09 +0200

    main aggiornato

--- main.py
+++ main.py
@@ -1,10 +1,9 @@
 import os
 import os.path
 
-def controlla_oggetto_in_categorie(oggetto):
+def controlla_categoria(oggetto):
     categorie = ["R1", "R2", "R3", "R4", "R5"]
     presente = False
-
     for categoria in categorie:
         file_categoria = categoria + ".txt"
         if os.path.isfile(file_categoria):
@@ -13,12 +12,14 @@ def controlla_oggetto_in_categorie(oggetto):
                 if oggetto.upper() in contenuto:
                     print(f"L'oggetto '{oggetto}' deve essere buttato nella categoria '{categoria}'.")
                     presente = True
-
+                    x = open('lista.txt', 'a')
+                    x.writelines(f"{oggetto.upper()}, {categoria} \n")
+                    
     if not presente:
         print(f"L'oggetto '{oggetto}' non è presente in nessuna categoria.")
 
 def main():
     oggetto = input("Inserisci il nome dell'oggetto da rifiutare: ")
-    controlla_oggetto_in_categorie(oggetto)
+    controlla_categoria(oggetto)
 
 main()
