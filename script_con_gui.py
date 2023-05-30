5e42d3b3c102dc476c17a54d7d0c0cdbff80242a
ROMANO ROBERTO <133423497+ExpRobi@users.noreply.github.com>
2023-05-30 12:54:46 +0200

    Modifica lo script con la gui
    Aggiunto il dialogo Programma - utente

--- script_con_gui.py
+++ script_con_gui.py
@@ -1,7 +1,9 @@
 import os
 import os.path
 import tkinter as tk
-from tkinter import messagebox
+from tkinter import messagebox, simpledialog
+import keyboard
+import getpass
 
 def controlla_categorie(oggetto):
     categorie = ["R1", "R2", "R3", "R4", "R5"]
@@ -20,7 +22,7 @@ def controlla_categorie(oggetto):
                     x.writelines(f"{oggetto.upper()}, {categoria}\n")
                     controlla_button.config(state="disabled")  # Disabilita il pulsante "Controlla"
                     oggetto_entry.config(state="disabled")  # Disabilita l'entry widget
-                    root.after(5000, abilita_controlla)
+                    root.after(10000, abilita_controlla)
                     break  # Esci dal ciclo se l'oggetto è stato trovato
 
     if not presente:
@@ -37,6 +39,35 @@ def controlla_oggetto(event=None):
 def chiudi_programma(event):
     root.quit()
 
+def salva_nome_utente(nome_utente):
+    with open("nome_utente.txt", "w") as file:
+        file.write(nome_utente)
+
+def leggi_nome_utente():
+    nome_utente = ""
+    if os.path.isfile("nome_utente.txt"):
+        with open("nome_utente.txt", "r") as file:
+            nome_utente = file.read()
+    return nome_utente
+
+def mostra_dialogo_benvenuto():
+    nome_utente = leggi_nome_utente()
+
+    if nome_utente == "":
+        nome_pc = getpass.getuser()
+        messagebox.showinfo("Benvenuto", f"Ciao Utente ({nome_pc}), è la prima volta che utilizzi il programma?")
+        risposta = messagebox.askyesno("Conferma", "È il tuo nome?")
+        if risposta:
+            nome_utente = nome_pc
+            salva_nome_utente(nome_utente)
+        else:
+            nome_utente = simpledialog.askstring("Inserisci il tuo nome", "Inserisci il tuo nome:")
+            salva_nome_utente(nome_utente)
+    else:
+        messagebox.showinfo("Benvenuto", f"Ciao, {nome_utente}!")
+
+    return nome_utente
+
 root = tk.Tk()
 root.title("Controllo Rifiuti")
 
@@ -45,11 +76,13 @@ logo_path = "logo.png"
 
 # Carica l'immagine del logo
 logo_image = tk.PhotoImage(file=logo_path)
-logo_image = logo_image.subsample(2)  # Ridimensiona l'immagine del logo (fattore di sottocampionamento 2)
+logo_image = logo_image.subsample(2)  # Ridimensiona l'immagine del logo
 
 # Crea una label per visualizzare il logo
 logo_label = tk.Label(root, image=logo_image)
 logo_label.pack()
+
+nome_utente = mostra_dialogo_benvenuto()
 
 oggetto_label = tk.Label(root, text="Inserisci il nome dell'oggetto da rifiutare:")
 oggetto_label.pack()
